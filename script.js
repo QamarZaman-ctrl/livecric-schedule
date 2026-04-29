6099a522e73c3d96b1070cdc4eea412e3c0d7a85
QamarZaman-ctrl <official.zamanjanjua@gmail.com>
2026-04-29 15:15:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -134,3 +134,10 @@ async function getFastSchedule() {
 }
 
 document.addEventListener('DOMContentLoaded', getFastSchedule);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered!'))
+      .catch(err => console.log('Service Worker Error', err));
+  });
+}
